52141ee3fb5542eeb354df6ff19fdca640fb59aa
Josias <87546094+Josyas@users.noreply.github.com>
2021-12-16 05:04:26 -0300

    app locale, number and date formatting

--- CadastroWebMvc/CadastroWebMvc/Startup.cs
+++ CadastroWebMvc/CadastroWebMvc/Startup.cs
@@ -12,7 +12,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CadastroWebMvc.Data;
 using CadastroWebMvc.Services;
-
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 namespace CadastroWebMvc
 {
     public class Startup
@@ -48,6 +49,16 @@ namespace CadastroWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS},
+                SupportedUICultures = new List<CultureInfo> { enUS}
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
